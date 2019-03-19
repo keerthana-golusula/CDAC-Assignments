f4724911e51bcc0ad02cbf157b1853c591e81da3
divishabyreddy <divishab.be19@uceou.edu>
2019-03-19 22:50:57 +0530

    checked three programs

--- c assigments/cassign_4/15.stringlibraries.c
+++ c assigments/cassign_4/15.stringlibraries.c
@@ -5,16 +5,27 @@ void str_cpy(char *,char *);
 int str_cmp(char *,char *);
 void str_cat(char *,char *);
 int main(){
-	char str[50],str2[50];
+	char string1[50],string2[50],string3[50];
    printf_s("enter the string\n");
-   scanf_s("%[^\n]s",str);// string input can contain spaces
-   str_cpy(str2,str);//copy of str into str2
-   printf_s("its copied into other string str2 is %s\n",str2);
-   str_rev(str);//reversal of str
-   printf_s("after reversal: %s\n",str);
+   scanf_s("%[^\n]s",string1);// string string can contain spaces
+     scanf_s("%[^\n]s",string3);
+   str_cpy(string2,string1);//copy of str into str2
+   printf_s("its copied into other string string2 is %s\n",string2);
+   str_rev(string1);//reversal of str
+   printf_s("after reversal: %s\n",string1);
    printf_s("concatinating string and its reversal\n");
-  str_cat(str2,str);// concatination of str with str2
-  printf("after concatenation: %s\n",str2);
+  str_cat(string2,string1);// concatination of str with str2
+  printf("after concatenation: %s\n",string2);
+  printf("enter string to be compared with string1:");
+  //scanf_s("%[^\n]s",string3);
+  getchar();
+  if(str_cmp(string3,string1)<0){
+  	printf("string3 is less than string1");
+  }else if(str_cmp(string3,string1)>0){
+  	printf("string3 is greater than string1");
+  }else{
+  	printf("string3 is equal to string1");
+  }
 }
 //reversal of string into the same string
 
@@ -48,9 +59,9 @@ int str_cmp(char *str1,char *str2){
 		}
 		++i;
 	if(str1[i]-str2[i]>0)
-       return -1;
-       else
        return 1;
+       else
+       return -1;
    }
 }
 
